1ada58b26cb362ab34de982cff36ac7766ea10a6
arnab <arnabchatterjee.ac.2@gmail.com>
2023-04-09 09:41:04 +0530

    Exception Handling -- returning 404 if user not found

--- api/resources/User/utils.py
+++ api/resources/User/utils.py
@@ -5,21 +5,19 @@ from api.utils.Token import verify_password
 from fastapi import HTTPException
 from pydantic import EmailStr
 
-def verify_user(db: Session, email: EmailStr,password:str):
-    user = db.query(User).filter(User.email == email).first()
-    received_password = password
-    exisiting_password = user.password
-    
-    if verify_password(received_password,exisiting_password):
-        return UserResult.from_orm(user)
-    
-    raise HTTPException(status_code=403)
-
 def get_user_by_id(db:Session,id:int):
-    return db.query(User).filter(User.id == id).first()
+    user = db.query(User).filter(User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=404)
+    return user
 
 def get_user_by_email(db:Session,email:EmailStr):
-    return db.query(User).filter(User.email==email).first()
+    user = db.query(User).filter(User.email==email).first()
+    print(user)
+    if not user:
+        raise HTTPException(status_code=404)
+    return user
+
 
 def create_new_user(db: Session, user: UserCreate):
     user_exists = get_user_by_email(db,email=user.email)
@@ -33,3 +31,13 @@ def create_new_user(db: Session, user: UserCreate):
     db.commit()
     db.refresh(new_user)
     return UserResult.from_orm(new_user)
+
+def verify_user(db: Session, email: EmailStr,password:str):
+    user = get_user_by_email(db,email)
+    received_password = password
+    exisiting_password = user.password
+    
+    if verify_password(received_password,exisiting_password):
+        return UserResult.from_orm(user)
+    
+    raise HTTPException(status_code=403)
