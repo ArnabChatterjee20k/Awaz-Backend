b60e9d111dd59c7c8a953ed762420032cef652e1
arnab <arnabchatterjee.ac.2@gmail.com>
2023-04-14 11:08:05 +0530

    added tracking or errors in dev mode

--- api/utils/get_error_info.py
+++ api/utils/get_error_info.py
@@ -1,5 +1,9 @@
-import os , sys 
+import os , sys  , traceback
+from api.Config import Config
 def get_error_info():
-    exc_type, exc_obj, exc_tb = sys.exc_info()
-    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    print(exc_type, fname, exc_tb.tb_lineno)
+    if not Config.PRODUCTION:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+
+        traceback.print_exc()
